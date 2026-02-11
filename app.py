f85f8b0177b0febc4e86c27597f954bfd4cf475f
Vineet kumar <vineet123koli@gmail.com>
2026-02-11 21:22:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Python Auto Deploy Works!"
+    return "Python Auto Deploy Works but this is 2nd commit!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
